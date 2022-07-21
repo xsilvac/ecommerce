814904c09bdc739dd80f991c96f1f737cd6efc14
ximena silva <ximenasilvacotrina11@gmail.com>
2022-07-20 21:23:17 -0500

    Creando el navbar. Close #2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './component/Home';
 import NoFound from './component/NoFound';
 import Product from './component/Product';
 import Products from './component/Products';
+import NavBar from './component/Navbar';
+import About from './component/About';
+import Contact from './component/Contact';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='Products' element={ <Products/> } />
-        <Route path='Products/:id' element={ <Product /> } />
-        <Route path='*' element={ <NoFound/> } />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='Products' element={<Products />} />
+            <Route path='Products/:id' element={<Product />} />
+            <Route path='About' element={<About />} />
+            <Route path='Contact' element={<Contact />} />
+            <Route path='*' element={<NoFound />} />
+          </Routes>
+      </BrowserRouter></>
   );
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import logo from '../img/newStyle.png'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <NavLink to='/' className="navbar-brand">
+                        <img src={logo} alt="logo" width="250" height="40"/>
+                    </NavLink >
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink to='/Productos' className="nav-link active" aria-current="page">Productos</NavLink >
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to='/About' className="nav-link">Nosotros</NavLink >
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to='/Contact' className="nav-link">Contactanos</NavLink >
+                            </li>
+                        </ul>
+                        <div className="d-flexbjh">
+                            <NavLink to='/' className="btn btn-outline-secondary">
+                            <i className='fa fa-sign-in me-1'></i>Iniciar</NavLink >
+                            <NavLink to='/' className="btn btn-outline-secondary ms-2">
+                            <i className='fa fa-user-plus me-1'></i>Registrate</NavLink >
+                            <NavLink to='/' className="btn btn-outline-secondary ms-2">
+                            <i className='fa fa-shopping-cart me-1'></i>Cart(0)</NavLink >
+                        </div>
+                    </div>
+                </div>
+            </nav>
+    )
 }
 
 export default Navbar
